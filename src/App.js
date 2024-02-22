d4783c44c9120262d541e11be27ae0b8fd174225
Thiru194 <thiruselvan0319@gmail.com>
2024-02-22 20:35:14 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React,{useState,useEffect} from 'react';
+import './App.css';
+  
+function App() {
+  const [quote,setQuote]= useState("");
+  const [author,setAuthor]=useState("");
+useEffect(()=>{
+  fetch("http://api.quotable.io/random")
+  .then(res => res.json())
+  .then(
+    (quote)=>{
+      setQuote(quote.content);
+      setAuthor(quote.author); 
+    }
+  )
+},[])
+let fetchNewQuote = ()=>{
+  fetch("http://api.quotable.io/random")
+  .then(res => res.json())
+  .then(
+    (quote)=>{
+      setQuote(quote.content);
+      setAuthor(quote.author);
+}
+  )
+}
+  return (
+   
+    <div className="App">
+      <div className='quote'>
+             <h2>{quote}</h2>
+             <small>-{author}-</small>
+    </div>
+    <button className='btn' onClick={fetchNewQuote}>Generate new Quote</button>
+    </div>
+  );
+}
+
+export default App;
